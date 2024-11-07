9c03b03f8120a0ad71cf27449f143ac04bda5159
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-07 13:58:52 +0100

    new posts look

--- project/src/main/resources/static/js/posts.js
+++ project/src/main/resources/static/js/posts.js
@@ -1,6 +1,5 @@
 
-const btn = document.getElementById("btn_search");
-const div_posts = document.getElementById("div_posts");
+const div_posts = document.getElementById("posts");
 
 function posts_clear(div_posts) {
     while (div_posts.firstChild) {
@@ -8,55 +7,79 @@ function posts_clear(div_posts) {
     }
 }
 
-function make_post_account(json) {
-    const post_account = document.createElement("a");
-    post_account.className = "post_account"
-    post_account.href = "/accounts/" + json.account.id;
-
-    const post_account_image = document.createElement("img");
-    post_account_image.className = "post_account_image";
-    post_account_image.src = json.account.avatar;
-    post_account.appendChild(post_account_image);
-
-    const post_account_userName = document.createElement("p");
-    post_account_userName.innerHTML = json.account.userName;
-    post_account_userName.className = "post_account_userName";
-    post_account.appendChild(post_account_userName);
-
-    return post_account;
-}
-
 function make_post_title(json) {
-    const post_title = document.createElement("a");
-    post_title.className = "post_title"
-    post_title.innerHTML = json.title;
-    post_title.href = "/posts/" + json.id;
+    const post_title = document.createElement("span");
+    post_title.className = "post_title";
+
     return post_title;
 }
 
 function make_post_content(json) {
 
     const post_content = document.createElement("div");
-    post_content.className = "post_content"
+    post_content.className = "post_content";
+
+    const post_title = document.createElement("span");
+    post_title.className = "post_title";
+    post_title.innerHTML = json.title;
+    post_content.appendChild(post_title);
 
     const post_text = document.createElement("span");
-    post_text.className = "post_text"
+    post_text.className = "post_text";
     post_text.innerHTML = json.text;
     post_content.appendChild(post_text);
 
     if (json.picture != null) {
-        const post_body_picture = document.createElement("img");
-        post_body_picture.className = "post_picture";
-        post_body_picture.src = json.picture;
-        post_content.appendChild(post_body_picture);
+        const post_picture = document.createElement("img");
+        post_picture.className = "post_picture";
+        post_picture.src = json.picture;
+        post_content.appendChild(post_picture);
     }
 
     return post_content;
 }
 
+function make_post_body(json) {
+
+    const post_body_link = document.createElement("a");
+    post_body_link.className = "post_body_link";
+    post_body_link.href = "/posts/" + json.id;
+
+    const post_body = document.createElement("div");
+    post_body.className = "post_body";
+    post_body.appendChild(make_post_content(json));
+
+    post_body_link.appendChild(post_body);
+    return post_body_link;
+}
+
+function make_post_account(json) {
+
+    const post_account = document.createElement("div");
+    post_account.className = "post_account"
+
+    const post_account_link = document.createElement("a");
+    post_account_link.className = "post_account_link"
+    post_account_link.href = "/accounts/" + json.account.id;
+
+    const post_account_image = document.createElement("img");
+    post_account_image.className = "post_account_image";
+    post_account_image.src = json.account.avatar;
+    post_account_link.appendChild(post_account_image);
+
+    const post_account_userName = document.createElement("p");
+    post_account_userName.innerHTML = json.account.userName;
+    post_account_userName.className = "post_account_userName";
+    post_account_link.appendChild(post_account_userName);
+
+    post_account.appendChild(post_account_link);
+
+    return post_account;
+}
+
 function make_post_controls(json) {
     const post_controls = document.createElement("div");
-    post_controls.className = "post_controls"
+    post_controls.className = "post_controls";
 
     const post_controls_like = document.createElement("button");
     post_controls_like.className = "post_controls_like";
@@ -68,6 +91,8 @@ function make_post_controls(json) {
     post_controls_comment.textContent = "💬 Comment";
     post_controls.appendChild(post_controls_comment);
 
+    post_controls.appendChild(make_post_account(json));
+
     return post_controls;
 }
 
@@ -78,9 +103,7 @@ function make_post(json) {
     const post = document.createElement("div");
     post.className = "post";
 
-    post.appendChild(make_post_account(json));
-    post.appendChild(make_post_title(json));
-    post.appendChild(make_post_content(json));
+    post.appendChild(make_post_body(json));
     post.appendChild(make_post_controls(json));
 
     return post;
@@ -89,7 +112,7 @@ function make_post(json) {
 async function load() {
     posts_clear(div_posts);
 
-    const sortOption = document.getElementById("sortOptions").value
+    const sortOption = document.getElementById("sortOptions").value;
     const url = `/api/posts?sort=${sortOption}`;
 
     const response = await fetch(url);
@@ -101,12 +124,3 @@ async function load() {
 }
 
 load();
-
-
-
-// SEARCH
-//   const txt_search = document.getElementById("txt_search");
-//   const btn_search = document.getElementById("btn_search");
-//   function search() { load_posts(txt_search.value); }
-//   txt_search.addEventListener("keydown", function(event) { if (event.key == 'Enter') { search(); } }, false);
-//   btn_search.onclick = function() { search(); }
