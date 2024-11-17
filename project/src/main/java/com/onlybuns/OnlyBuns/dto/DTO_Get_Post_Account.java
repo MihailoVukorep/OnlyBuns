def48805fa378d7e8e512dba8bc5d1fec1774157
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-17 02:14:29 +0100

    remove roles from get_post_account

--- project/src/main/java/com/onlybuns/OnlyBuns/dto/DTO_Get_Post_Account.java
+++ project/src/main/java/com/onlybuns/OnlyBuns/dto/DTO_Get_Post_Account.java
@@ -1,10 +1,7 @@
 package com.onlybuns.OnlyBuns.dto;
 
 import com.onlybuns.OnlyBuns.model.Account;
-import com.onlybuns.OnlyBuns.model.Role;
 import lombok.*;
-
-import java.util.Set;
 
 @Getter
 @Setter
@@ -15,12 +12,10 @@ public class DTO_Get_Post_Account {
     public Long id;
     public String userName;
     public String avatar;
-    public Set<Role> roles;
 
     public DTO_Get_Post_Account(Account account) {
         this.id = account.getId();
         this.userName = account.getUserName();
         this.avatar = account.getAvatar();
-        this.roles = account.getRoles();
     }
 }
