a39ce40807f48d0c6f9ea4b018d9400c004b1b0c
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-17 02:22:26 +0100

    removed email from dto_get_user -- not needed

--- project/src/main/java/com/onlybuns/OnlyBuns/dto/DTO_Get_User.java
+++ project/src/main/java/com/onlybuns/OnlyBuns/dto/DTO_Get_User.java
@@ -4,14 +4,12 @@ import com.onlybuns.OnlyBuns.model.Account;
 
 public class DTO_Get_User {
     public Long id;
-    public String email;
     public String userName;
     public String avatar;
     public boolean isAdmin;
 
     public DTO_Get_User(Account account) {
         this.id = account.getId();
-        this.email = account.getEmail();
         this.userName = account.getUserName();
         this.avatar = account.getAvatar();
         this.isAdmin = account.isAdmin();
