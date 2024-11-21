65bcf6d056f255e648a567720ede6fe51e7e541e
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-21 17:26:18 +0100

    new clock colors - will change them maybe

--- project/src/main/resources/static/js/clock.js
+++ project/src/main/resources/static/js/clock.js
@@ -7,6 +7,10 @@ function setup() {
 	angleMode(DEGREES);
 }
 
+function clock_color_sec()  { stroke(46, 204, 113);  }
+function clock_color_min()  { stroke(90, 192, 204);  }
+function clock_color_hour() { stroke(174, 129, 255); }
+
 function draw() {
 	clear();
 	translate(200, 200);
@@ -31,38 +35,38 @@ function draw() {
 	noFill();
 
 	// sec
-	stroke(255, 100, 150);
+	clock_color_sec();
 	let secondAngle = map(sc, 0, 60, 0, 360);
 	arc(0, 0, dis, dis, 0, secondAngle);
 	
 	// min
-	stroke(150, 100, 255);
+	clock_color_min();
 	let minuteAngle = map(mn, 0, 60, 0, 360);
 	arc(0, 0, dis-disf, dis-disf, 0, minuteAngle);
 	
 	// hour
-	stroke(150, 255, 100);
+	clock_color_hour();
 	let hourAngle = map(hr % 12, 0, 12, 0, 360);
 	arc(0, 0, dis-disf*2, dis-disf*2, 0, hourAngle);
 	
 	push();
 	strokeWeight(3);
 	rotate(secondAngle);
-	stroke(255, 100, 150);
+	clock_color_sec();
 	line(0, 0, 125, 0);
 	pop();
 	
 	push();
 	strokeWeight(6);
 	rotate(minuteAngle);
-	stroke(150, 100, 255);
+	clock_color_min();
 	line(0, 0, 100, 0);
 	pop();
 	
 	push();
 	strokeWeight(9);
 	rotate(hourAngle);
-	stroke(150, 255, 100);
+	clock_color_hour();
 	line(0, 0, 75, 0);
 	pop();
 	
