2fd604e1d5b9fa8e3860ace28e13127698159405
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-21 16:05:45 +0100

    aspect note

--- project/src/main/java/com/onlybuns/OnlyBuns/controller/aspect/Aspect_Main.java
+++ project/src/main/java/com/onlybuns/OnlyBuns/controller/aspect/Aspect_Main.java
@@ -24,9 +24,11 @@ public class Aspect_Main {
     public void controllerMethods() {
     }
 
+    // NOTE: all Controller classes must have Session and Model as args so this runs
     @Before("controllerMethods() && args(session, model, ..)") // Apply to all methods with session and model as args
     public void addAccountToModel(HttpSession session, Model model) {
         Account user = (Account) session.getAttribute("user");
+        // setting the user model for drawing navbar for all pages
         model.addAttribute("user", user == null ? null : new DTO_Get_User(service_account.lazy(user.getId())));
     }
 }
