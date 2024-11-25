4b3a74b21ff3cd50c9ac4b10aee1a9b0fadf0de5
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-11-25 11:18:52 +0100

    added frontend for follow/message

---
+++ project/src/main/resources/static/js/account.js
@@ -0,0 +1,32 @@
+function drawFollowButton(button, following) {
+    if (following) {
+        button.textContent = `✅ Following`;
+        button.classList.add("following");
+    }
+    else {
+        button.textContent = `➕ Follow`;
+        button.classList.remove("following");
+    }
+} 
+async function follow(id) {
+    const response = await fetch(`/api/accounts/${id}/follow`, { method: "POST" });
+    const response_text = await response.text();
+
+    const btn_follow = document.getElementById("btn_follow");
+
+    if (response.ok) {
+        if      (response_text == "Followed.")   { drawFollowButton(btn_follow, true);  }
+        else if (response_text == "Unfollowed.") { drawFollowButton(btn_follow, false); }
+        else                                     { popup(`✅ ${response_text}`);    }
+    }
+    else {
+        popup(`❌ ${response_text}`);
+    }
+
+    // TODO: maybe refresh page so the counter updates, or do it manually
+}
+
+
+function message(id) {
+    window.location.href = `/accounts/${id}/message`;
+}
