e1b717d1a185cefbdf8c620eb9f85c44cb575338
MihailoVukorep <mihailo.vukorep@gmail.com>
2024-12-09 03:08:19 +0100

    actually fixed iframe navigation

--- project/src/main/resources/static/js/iframe_init.js
+++ project/src/main/resources/static/js/iframe_init.js
@@ -1,12 +1,12 @@
-function iframe_resize(iframe) { iframe.style.height = iframe.contentWindow.document.body.scrollHeight + "px"; }
+function iframe_resize(iframe) { iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 50) + "px"; }
 
 function iframe_init(iframe_id) {
     const iframe = document.getElementById(iframe_id);
     iframe.onload = function() {
-        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-        iframeDoc.querySelectorAll('a').forEach(link => {
-            link.setAttribute('target', '_top');
-        });
+        // const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        // iframeDoc.querySelectorAll('a').forEach(link => {
+        //     link.setAttribute('target', '_top');
+        // });
 
         iframe_resize(iframe)
     };
