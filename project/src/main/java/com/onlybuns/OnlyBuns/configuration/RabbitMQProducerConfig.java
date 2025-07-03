d095e56a172cdbf5ba3ac97cb745ad9b2310f5a4
Ranka Radulovic <radulovic.in16.2021@uns.ac.rs>
2025-07-03 23:41:36 +0200

    [KT-3] RabbitMQ producer completed fanout exchange

--- project/src/main/java/com/onlybuns/OnlyBuns/configuration/RabbitMQProducerConfig.java
+++ project/src/main/java/com/onlybuns/OnlyBuns/configuration/RabbitMQProducerConfig.java
@@ -4,7 +4,9 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQProducerConfig {
     @Bean
     public Jackson2JsonMessageConverter converter() {
--- project/src/main/java/com/onlybuns/OnlyBuns/util/constants/SendPostConstants.java
+++ project/src/main/java/com/onlybuns/OnlyBuns/util/constants/SendPostConstants.java
@@ -1,9 +1,8 @@
 package com.onlybuns.OnlyBuns.util.constants;
 
 public class SendPostConstants {
-    public final String QUEUE_CREATE = "queue";
 
     public static final String EXCHANGE = "advertising";
 
-    public static final String ROUTING_KEY_CREATE = "send";
+    public static final String ROUTING_KEY_CREATE = "";
 }
